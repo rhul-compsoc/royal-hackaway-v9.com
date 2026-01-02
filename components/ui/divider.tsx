4dbf0f069ea78931fef2e0d189c61528ec57f054
Joseph Beck <joebeck03@icloud.com>
2026-01-02 13:30:41 +0000

    feat: start rewrite

--- components/ui/divider.tsx
+++
@@ -1,5 +0,0 @@
-export const Divider = (): JSX.Element => {
-  return (
-    <hr className="my-16 md:my-32 mx-auto h-1 border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-50 w-1/2" />
-  );
-};
