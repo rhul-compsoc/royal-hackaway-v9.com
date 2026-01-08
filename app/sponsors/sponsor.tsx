eea53aedd6933c0daec3fdaaa88878e3deb34dc8
Joseph Beck <joebeck03@icloud.com>
2026-01-08 23:50:37 +0000

    chore: update to gradient styling

--- app/sponsors/sponsor.tsx
+++ app/sponsors/sponsor.tsx
@@ -22,12 +22,12 @@ const sizeClasses: Record<NonNullable<SponsorProps['size']>, string> = {
 }
 
 const tierGradients: Record<NonNullable<SponsorProps['tier']>, string> = {
-  platinum: 'from-teal-300 via-teal-500 to-teal-300',
-  gold: 'from-yellow-400 via-yellow-600 to-yellow-400',
-  silver: 'from-slate-200 via-slate-400 to-slate-200',
-  bronze: 'from-amber-900 via-amber-700 to-amber-500',
-  supporter: 'from-purple-400 via-purple-600 to-purple-400',
-  partner: 'from-blue-400 via-orange-600 to-orange-400',
+  platinum: 'bg-linear-to-r from-teal-300 via-teal-500 to-teal-300',
+  gold: 'bg-linear-to-r from-yellow-400 via-yellow-600 to-yellow-400',
+  silver: 'bg-linear-to-r from-slate-200 via-slate-400 to-slate-200',
+  bronze: 'bg-linear-to-r from-amber-900 via-amber-700 to-amber-500',
+  supporter: 'bg-linear-60 from-orange-400 via-purple-600 to-orange-400',
+  partner: 'bg-linear-60 from-blue-400 via-orange-600 to-blue-400',
 }
 
 const Sponsor = ({ img, alt, href, className, tier, size = 'md' }: SponsorProps): ReactElement => {
@@ -42,7 +42,7 @@ const Sponsor = ({ img, alt, href, className, tier, size = 'md' }: SponsorProps)
       {isDefined(tier) ? (
         <div
           className={cn(
-            'bg-linear-to-r p-2 rounded-xl shadow-2xl hover:scale-105 transition-transform duration-300 hover:shadow-4xl',
+            'p-2 rounded-xl shadow-2xl hover:scale-105 transition-transform duration-300 hover:shadow-4xl',
             tierGradients[tier],
           )}
         >
