682d847189def07a22d9ad1b59e1f6d3a6cb5c4b
Joseph Beck <joebeck03@icloud.com>
2026-01-24 15:55:31 +0000

    chore: fmt

--- components/timetable/timetable-event.tsx
+++ components/timetable/timetable-event.tsx
@@ -11,7 +11,9 @@ interface TimetableEventProps {
 
 const TimetableEvent = ({ event, key }: TimetableEventProps): ReactElement => (
   <div key={key} className="flex items-stretch gap-4 px-6 py-4 transition-colors duration-200 hover:bg-primary/5">
-    <span className="font-bold text-primary whitespace-nowrap min-w-18 flex items-center justify-center">{event.startTime}</span>
+    <span className="font-bold text-primary whitespace-nowrap min-w-18 flex items-center justify-center">
+      {event.startTime}
+    </span>
     <div className="w-px bg-neutral-light/50" />
     <div className="text-text flex items-center">{isDefined(event.content) ? <event.content /> : undefined}</div>
   </div>
